a9ede00110b3d3e7b30cc3a08add194509476d97
Truong Hoang Long <146976574+LongHT25@users.noreply.github.com>
2026-04-21 23:35:19 +0700

    Fix: sửa lỗi flake8 lint

--- BT1_QEMU_SystemMonitor/system_monitor.py
+++ BT1_QEMU_SystemMonitor/system_monitor.py
@@ -33,7 +33,7 @@ try:
             f"[{now}] CPU: {cpu_avg:.1f}% | "
             f"RAM: {ram_used_mb}/{ram_total_mb} MB ({ram_pct}%) | "
             f"Disk: {disk_pct}% | {status}"
-)
+        )
         print(line)
 
         # Bước 7: In dòng cảnh báo riêng nếu cảnh báo
